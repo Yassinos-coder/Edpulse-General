b87eecf5eb9fe0acb4a2e5bf330e1c5a0cac5268
Yassinos <37705952+Yassinos-coder@users.noreply.github.com>
2025-05-05 13:13:37 +0100

    Updating code

---
+++ jquery_fluent_forms.js
@@ -0,0 +1,7 @@
+$form.find('#ff_90_2_file_upload_releve_de_note_2')
+  .closest('.ff-el-input--content')
+  .before(`
+    <div style="font-size: 0.7em; font-style: italic; color: #023047; margin: 0px 0 7px;">
+      *Merci de télécharger votre relevé de notes le plus récent. Il est également préférable de joindre votre relevé de notes du secondaire.
+    </div>
+  `);
