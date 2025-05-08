b5f8513d82a34ac84e421c5989668acd0972fc48
Yassinos <37705952+Yassinos-coder@users.noreply.github.com>
2025-05-08 17:08:33 +0100

    Update in code

--- add_applicants_to_supabase.js
+++ add_applicants_to_supabase.js
@@ -1,29 +1,49 @@
 const supabaseKey = "{{supabasePublicKeyEdpulse}}";
 const supabaseUrl = "{{supabaseProjectUrl}}";
+
 const data = {
-firstname: "1. Names First Name : Yassine",
-lastname: "1. Names Last Name : Castro",
-email: "1. Email : yassine.castro@edpulse.com",
-phonenumber: "1. Phone Number : +212654593277",
-user_ip_address: "1. Submission Ip : 105.156.1.174",
-branch: "1. Branch : tutorax_qc",
-submission_status: "1. Submission Status : newly_submitted"
+    firstname: "1. Names First Name : Yassine",
+    lastname: "1. Names Last Name : Castro",
+    email: "1. Email : yassine.castro@edpulse.com",
+    phonenumber: "1. Phone Number : +212654593277",
+    user_ip_address: "1. Submission Ip : 196.217.153.0",
+    branch: "1. Branch : tutorax_qc",
+    submission_status: "newly_submitted"
 };
 
 async function addContractorData() {
-const response = await fetch(`${supabaseUrl}/rest/v1/new_applicants`, {
-method: 'POST',
-headers: {
-'Content-Type': 'application/json',
-'apikey': supabaseKey,
-'Authorization': `Bearer ${supabaseKey}`,
-'Content-Profile': 'kpis',
-'Accept-Profile': 'kpis'
-  }
-});
-  const result = await response;
-return result;
+    try {
+        const response = await fetch(`${supabaseUrl}/rest/v1/new_applicants`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'apikey': supabaseKey,
+                'Authorization': `Bearer ${supabaseKey}`,
+                'Content-Profile': 'kpis',
+                'Accept-Profile': 'kpis'
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            return {
+                status: "success",
+                message: "Contractor data submitted successfully!"
+            };
+        } else {
+            const errorDetails = await response.json();
+            return {
+                status: "error",
+                message: "Submission failed",
+                details: errorDetails
+            };
+        }
+    } catch (error) {
+        return error.message
+
+    }
 }
 
-const result = await addContractorData();
-return result;
+addContractorData().then(result => {
+    console.log(result); // Or return it if this is in a serverless function
+});
--- code_step_email_temp_by_branch.js
+++ code_step_email_temp_by_branch.js
@@ -2,60 +2,65 @@ const BRANCH = "3268";
 
 function mapBranchToResources() {
     const branchResources = {
-        3268: {
+        3268: { // TUTORAT FR
             driveLink:
                 "https://drive.google.com/file/d/1iWiTMiGGnCZK9cHQM_DnijlhUh-SmHrm/view?usp=sharing",
             quizLink:
                 "https://docs.google.com/forms/d/e/1FAIpQLSd12X3DGH3BDn7hvjGabm72YJ-M54ss0wcTNGE_b1OHyiKv5w/viewform?usp=pp_url",
             profileLink:
-                "https://drive.google.com/uc?export=download&id=1N3iUSLC_EO6uf78yvEJYH3LgGn7WgwSN",
+                "https://tutorcruncher-public.s3.amazonaws.com/tutorax/Julie.png",
             contact: "Julie Morin",
             guide_phrase: "Télécharger et lire le Guide des tuteurs en appuyant",
             emailInbox: "cv@tutorax.com",
+            role: "Assistante au recrutement / Recruitment Assistant"
         },
-        8427: {
+        8427: { // TUTORAT Orthopédagogie
             driveLink:
                 "https://drive.google.com/uc?export=download&id=18aT6qVvxjN2Q3DT9orWbN2ozLGaXatDb",
             quizLink:
                 "https://docs.google.com/forms/d/e/1FAIpQLSd-SxdFq1aZL5tsKpW9Rut-j8v2jNHq5MvQXkTnxygFJ065xg/viewform?usp=pp_url",
             profileLink:
-                "https://drive.google.com/uc?export=download&id=1_BWjPvUDtU5L_kxkIMFh2mAoOj2Qx4Z0",
+                "https://tutorcruncher-public.s3.amazonaws.com/tutorax/Justine.png",
             contact: "Julie Morin",
             guide_phrase: "Télécharger et lire le Guide des intervenants en appuyant",
             emailInbox: "cv.orthopedagogue@tutorax.com",
+            role: "Assistante au recrutement / Recruitment Assistant"
         },
-        5737: {
+        5737: {  // TUTORAT Stimulation du langage
             driveLink:
                 "https://drive.google.com/file/d/1-4sqvH4ikha2at5FVzDSRUz6qQunArHc/view?usp=sharing",
             quizLink:
                 "https://docs.google.com/forms/d/e/1FAIpQLSf-fFgTMFM00ee6skB3KmvM4PoXHEXxQCS1R2TOIpDN2q3XaA/viewform?usp=pp_url",
             profileLink:
-                "https://drive.google.com/uc?export=download&id=1Sk0C7DX32eom8ijwbJbULm92qGGqsgZS",
+                "https://tutorcruncher-public.s3.amazonaws.com/tutorax/Justine.png",
             contact: "Julie Morin",
             guide_phrase: "Lire le Guide des ASL en appuyant",
             emailInbox: "cv.asl@tutorax.com",
+            role: "Assistante au recrutement / Recruitment Assistant"
         },
-        7673: {
+        7673: { // TUTORAT Canada
             driveLink:
                 "https://drive.google.com/file/d/1tx77qV_qpy7FeSWH_cFr0gOCD3YFalie/view?usp=sharing",
             quizLink:
                 "https://docs.google.com/forms/d/e/1FAIpQLSfLFfKS9B2JkPZlr5w8EyN8jua41V0nkSbxAy-sdbu05sDgKw/viewform?usp=pp_url",
             profileLink:
-                "https://drive.google.com/uc?export=download&id=1T4o25UrJVsh0N8THTf7Mo2q-tcrZ3W8S",
+                "https://tutorcruncher-public.s3.amazonaws.com/tutorax/Melissa.png",
             contact: "Melissa Davidson",
             guide_phrase: "Read the Tutor's guide by clicking",
             emailInbox: "cv.tutor@tutorax.com",
+            role: "Recruitment Assistant / Assistante au recrutement "
         },
-        15751: {
+        15751: { // TUTORAT USA
             driveLink:
                 "https://drive.google.com/file/d/1pR-fdC13NrtfsbQooVd5TdO0xilqsrCH/view?usp=sharing",
             quizLink:
                 "https://docs.google.com/forms/d/e/1FAIpQLSdOcsP2CWvIyKr2bM7-OP9C5ldd0Kkkd8SXvkfCOeTDRbiRLQ/viewform?usp=pp_url",
             profileLink:
-                "https://drive.google.com/uc?export=download&id=1S3XO472OlRWXbNq46hC1cM3BxXl_6_5o",
+                "https://tutorcruncher-public.s3.amazonaws.com/tutorax/Amanda.png",
             contact: "Amanda Evans",
             guide_phrase: "Read the Tutor's guide by clicking",
             emailInbox: "tutors.us@tutorax.com",
+            role: "Recruitment Assistant"
         },
     };
 
