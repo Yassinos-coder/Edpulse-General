b0ebc9e406433f68e5eaadbf11e614026792d542
Yassinos <37705952+Yassinos-coder@users.noreply.github.com>
2025-05-12 20:59:52 +0100

    Updates were made

--- add_applicants_to_supabase.js
+++ add_applicants_to_supabase.js
@@ -1,6 +1,7 @@
 const supabaseKey = "{{supabasePublicKeyEdpulse}}";
 const supabaseUrl = "{{supabaseProjectUrl}}";
 
+
 const data = {
     firstname: "1. Names First Name : Yassine",
     lastname: "1. Names Last Name : Castro",
@@ -8,7 +9,9 @@ const data = {
     phonenumber: "1. Phone Number : +212654593277",
     user_ip_address: "1. Submission Ip : 196.217.153.0",
     branch: "1. Branch : tutorax_qc",
-    submission_status: "newly_submitted"
+    submission_status: "newly_submitted",
+    application_source: "Site web",
+    time_of_cv_submission: new Date().toISOString(),
 };
 
 async function addContractorData() {
@@ -26,24 +29,17 @@ async function addContractorData() {
         });
 
         if (response.ok) {
-            return {
-                status: "success",
-                message: "Contractor data submitted successfully!"
-            };
+            const data = await response.json();
+            return { success: true, response: data };
         } else {
             const errorDetails = await response.json();
-            return {
-                status: "error",
-                message: "Submission failed",
-                details: errorDetails
-            };
+            return { success: false, error: errorDetails };
+
         }
     } catch (error) {
-        return error.message
+        return { success: false, error: error.message || error.toString() };
 
     }
 }
 
-addContractorData().then(result => {
-    console.log(result); // Or return it if this is in a serverless function
-});
+return addContractorData();
